1011b21e47f5b2724a8088bbfb08f59ba7636a8e
Said Maadan <me@saidmaadan.com>
2017-06-17 10:41:53 -0500

    setup user profile provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,4 +12,8 @@ export class HomePage {
 
   }
 
+  goToProfile(){
+    this.navCtrl.push('profile');
+  }
+
 }
--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -1,18 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import firebase from 'firebase';
 
-/*
-  Generated class for the ProfileProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class ProfileProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello ProfileProvider Provider');
+  public userProfile:firebase.database.Reference;
+  public currentUser:firebase.User;
+
+  constructor() {
+    this.currentUser = firebase.auth().currentUser;
+    this.userProfile = firebase.database().ref('/userProfile')
+      .child(firebase.auth().currentUser.uid);
   }
 
+  getUser(): firebase.database.Reference{
+    return this.userProfile;
+  }
+
+  updateName(firstName: string, lastName: string): firebase.Promise<void>{
+    return this.userProfile.update({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  updateDOB(birthDate: string): firebase.Promise<any>{
+    return this.userProfile.update({
+      birthDate: birthDate
+    });
+  }
+
+  updateEmail(newEmail: string, password: string): firebase.Promise<any>{
+    const credential = firebase.auth.EmailAuthProvider
+      .credential(this.currentUser.email, password);
+    return this.currentUser.reauthenticateWithCredential(credential)
+      .then(user => {
+        this.currentUser.updateEmail(newEmail).then(user => {
+          this.userProfile.update({email: newEmail});
+        });
+      });
+  }
+
+  updatePassword(newPassword: string, oldPassword: string): firebase.Promise<any> {
+    const credential =  firebase.auth.EmailAuthProvider
+      .credential(this.currentUser.email, oldPassword);
+
+    return this.currentUser.reauthenticateWithCredential(credential).then( user => {
+      this.currentUser.updatePassword(newPassword).then( user => {
+        console.log("Password Changed");
+      }, error => {
+        console.log(error);
+      });
+    });
+  }
 }
