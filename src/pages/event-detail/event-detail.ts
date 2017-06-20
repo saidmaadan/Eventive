f2d9b05c87aa3bb64f31f27d4f4e93cf001d48db
Said Maadan <me@saidmaadan.com>
2017-06-20 12:23:45 -0500

    Added capabilities for user to take picture with phone camera

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EventProvider } from '../..//providers/event/event';
+import { Camera } from '@ionic-native/camera';
 
 @IonicPage({
   name: 'event-detail'
@@ -11,12 +12,16 @@ import { EventProvider } from '../..//providers/event/event';
 })
 export class EventDetailPage {
 
-  public currentEvent: any;
+  public currentEvent: any = {};
+  public guestName: string = '';
+  public guestPicture: string = null;
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public eventProvider: EventProvider
+    public eventProvider: EventProvider,
+    public camera: Camera
   ) {
   }
 
@@ -27,5 +32,31 @@ export class EventDetailPage {
         this.currentEvent.id = eventSnapshot.key;
       });
   }
+
+  addGuest(guestName) {
+    this.eventProvider.addGuest(guestName, this.currentEvent.id,
+      this.currentEvent.price, this.guestPicture).then(() => {
+        this.guestName = '';
+        this.guestPicture = null;
+      });
+  }
+
+  takePicture(){
+      this.camera.getPicture({
+        quality : 95,
+        destinationType : this.camera.DestinationType.DATA_URL,
+        sourceType : this.camera.PictureSourceType.CAMERA,
+        allowEdit : true,
+        encodingType: this.camera.EncodingType.PNG,
+        targetWidth: 500,
+        targetHeight: 500,
+        saveToPhotoAlbum: true
+      }).then(imageData => {
+        this.guestPicture = imageData;
+      }, error => {
+        console.log("ERROR -> " + JSON.stringify(error));
+      });
+    }
+
 
 }
--- src/providers/event/event.ts
+++ src/providers/event/event.ts
@@ -17,6 +17,7 @@ export class EventProvider {
         date: eventDate,
         price: eventPrice * 1,
         cost: eventCost * 1,
+        revenue: eventCost * -1
       });
     }
 
@@ -28,4 +29,24 @@ export class EventProvider {
       return this.userProfileRef.child('/eventList').child(eventId);
     }
 
+    addGuest(guestName, eventId, eventPrice, guestPicture = null): firebase.Promise<any>{
+      return this.userProfileRef.child('/eventList').child(eventId)
+        .child('guestList').push({
+          guestName: guestName
+        })
+        .then((newGuest) => {
+        this.userProfileRef.child('/eventList').child(eventId).transaction( event => {
+          event.revenue += eventPrice;
+          return event;
+        });
+        if (guestPicture != null) {
+        firebase.storage().ref('/guestProfile/').child(newGuest.key)
+        .child('profilePicture.png').putString(guestPicture, 'base64', {contentType: 'image/png'})
+        .then((savedPicture) => {
+          this.userProfileRef.child('/eventList').child(eventId).child('guestList')
+          .child(newGuest.key).child('profilePicture').set(savedPicture.downloadURL);
+        });
+      }
+    });
+  }
 }
